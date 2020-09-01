f98efaf96cf18ca8160903e02c3c4839a627f96f
Indrajeet Kalra <Inder@Indrajeets-MacBook-Pro.local>
2020-09-01 17:31:12 +1000

    Added spring cloud Eureka client library to enable service registry

--- src/main/java/com/goodbooks/bookrating/BookratingApplication.java
+++ src/main/java/com/goodbooks/bookrating/BookratingApplication.java
@@ -2,8 +2,10 @@ package com.goodbooks.bookrating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookratingApplication {
 
 	public static void main(String[] args) {
